6207e0d286065ade74c4620bd87d4c5c04a2bee8
sarveshkulkarni7 <skulka19@asu.edu>
2017-10-27 22:18:10 +0530

    Sprint 3

---
+++ PiviPlugin.diagram/src/asu/ser/capstone/pivi/diagram/providers/assistants/PiviModelingAssistantProviderOfThreadEndEditPart.java
@@ -0,0 +1,24 @@
+package asu.ser.capstone.pivi.diagram.providers.assistants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+
+import asu.ser.capstone.pivi.diagram.providers.PiviElementTypes;
+import asu.ser.capstone.pivi.diagram.providers.PiviModelingAssistantProvider;
+
+public class PiviModelingAssistantProviderOfThreadEndEditPart extends PiviModelingAssistantProvider{
+	
+	
+	@Override
+	public List<IElementType> getTypesForPopupBar(IAdaptable host) {
+		List<IElementType> types = new ArrayList<IElementType>(3);
+		types.add(PiviElementTypes.InputPort_3001);
+		types.add(PiviElementTypes.OutputPort_3003);
+		types.add(PiviElementTypes.StartPort_3002);
+		return types;
+	}
+
+}
